e1025de89bf97640bd33301af23be7c002e9ab67
thiago <thiago12lopesdeoliveira@gmail.com>
2026-08-10 13:08:40 -0300

    enunciado do problema

--- src/main/scala/scalabasico/exercicios/TaxaCambio.scala
+++ src/main/scala/scalabasico/exercicios/TaxaCambio.scala
@@ -1,5 +1,7 @@
 package scalabasico.exercicios
 
+// Utilizando os conceitos de interpolação e formatação de strings (interpolador f), defina uma função que receba um valor em Reais (BRL) e a taxa de câmbio de conversão para Dólares (USD), e retorne uma string formatada no seguinte padrão: "R$ XXX.XX equivalem a US$ YYY.YY". Ambos os valores numéricos devem ser apresentados obrigatoriamente com exatamente duas casas decimais.
+
 @main def taxaCambio(): Unit =
     def conversao(reais: Float, taxa: Float): String =
         val dolares = reais / taxa
