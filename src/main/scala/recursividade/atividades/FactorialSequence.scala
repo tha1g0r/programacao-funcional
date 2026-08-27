b1031dc5584cf57043d45ebe93947c778d259b52
thiago <thiago12lopesdeoliveira@gmail.com>
2026-08-27 10:14:21 -0300

    final FactorialSequence

--- src/main/scala/recursividade/atividades/FactorialSequence.scala
+++ src/main/scala/recursividade/atividades/FactorialSequence.scala
@@ -1,5 +1,8 @@
 package recursividade.atividades
 
+import scala.collection.mutable
+import scala.annotation.tailrec
+
 /**
  * Computes the factorial sequence from 0! to n! using a naive recursive approach.
  * 
@@ -16,6 +19,7 @@ def factorialSequenceNaive(n: Int): List[BigInt] = {
         if (i <= 1) 1
         else i * factorial(i - 1)
     }
+
     (0 to n).map(factorial).toList
 }
 
@@ -31,7 +35,12 @@ def factorialSequenceNaive(n: Int): List[BigInt] = {
  * 
  * Example: factorialSequenceMemoized(4, mutable.Map()) should return [1, 1, 2, 6, 24]
  */
-def factorialSequenceMemoized(n: Int, memo: scala.collection.mutable.Map[Int, BigInt]): List[BigInt] = ???
+def factorialSequenceMemoized(n: Int, memo: mutable.Map[Int, BigInt]): List[BigInt] =
+    def factorialMemoized(i: Int): BigInt =
+        if i <= 1 then 1
+        else memo.getOrElseUpdate(i, i * factorialMemoized(i - 1))
+    
+    (0 to n).map(factorialMemoized).toList
 
 /**
  * Computes the factorial sequence from 0! to n! using tail recursion for optimal performance.
@@ -49,7 +58,10 @@ def factorialSequenceMemoized(n: Int, memo: scala.collection.mutable.Map[Int, Bi
  * Example: factorialSequenceTailRecursive(4) should return [1, 1, 2, 6, 24]
  */
 def factorialSequenceTailRecursive(n: Int, i: BigInt = 0, acc: BigInt = 1, result: List[BigInt] = Nil): List[BigInt] =
-    ???
+    if i > n then result
+    else
+        val proximoAcc = if i == 0 then acc else acc * i
+        factorialSequenceTailRecursive(n, i + 1, proximoAcc, result :+ proximoAcc)
 
 /**
  * Main entry point for testing the factorial sequence implementations.
@@ -57,4 +69,14 @@ def factorialSequenceTailRecursive(n: Int, i: BigInt = 0, acc: BigInt = 1, resul
  * This function can be used to test and compare the different implementations
  * of the factorial sequence computation.
  */
-@main def mainFactorialSequence(): Unit = {}
+@main def mainFactorialSequence(): Unit = {
+    // factorialSequenceNaive
+    println(factorialSequenceNaive(5))
+
+    // factorialSequenceMemoized
+    val memo = mutable.Map[Int, BigInt]()
+    println(factorialSequenceMemoized(5, memo))
+
+    // factorialSequenceTailRecursive
+    println(factorialSequenceTailRecursive(5))
+}
