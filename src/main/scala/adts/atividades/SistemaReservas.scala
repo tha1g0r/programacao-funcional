8fc9da569892b489d5454960863b3de86a5ef94d
thiago <thiago12lopesdeoliveira@gmail.com>
2026-09-01 23:04:33 -0300

    parte 1 do projeto

--- src/main/scala/adts/atividades/SistemaReservas.scala
+++ src/main/scala/adts/atividades/SistemaReservas.scala
@@ -12,5 +12,28 @@ case class SolicitacaoReserva(
   solicitante: String
 )
 
+// Tratamento de erros
+
+enum ErroReserva:
+    case HorarioInvalido
+    case SalaLotada
+    case SolicitanteInvalido
+
+def validarSolicitacao(s: SolicitacaoReserva): Either[ErroReserva, SolicitacaoReserva] =
+    val validarHorario: SolicitacaoReserva => Either[ErroReserva, SolicitacaoReserva] =
+        s => if s.horario.inicio >= s.horario.fim || s.horario.inicio < 0 || s.horario.fim > 23 then Left(ErroReserva.HorarioInvalido) else Right(s)
+
+    val validarCapacidade: SolicitacaoReserva => Either[ErroReserva, SolicitacaoReserva] =
+        s => if s.numParticipantes > s.sala.capacidade then Left(ErroReserva.SalaLotada) else Right(s)
+
+    val validarSolicitante: SolicitacaoReserva => Either[ErroReserva, SolicitacaoReserva] =
+        s => if s.solicitante.isBlank then Left(ErroReserva.SolicitanteInvalido) else Right(s)
+
+    for
+        s1 <- validarHorario(s)
+        s2 <- validarCapacidade(s1)
+        s3 <- validarSolicitante(s2)
+    yield s3
+
 @main def mainSistemaReservas(): Unit =
     ???
