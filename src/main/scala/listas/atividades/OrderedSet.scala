ce76814fe5763011dca20bb0d57c3fc6d012b32c
thiago <thiago12lopesdeoliveira@gmail.com>
2026-09-06 12:13:46 -0300

    intersect function

--- src/main/scala/listas/atividades/OrderedSet.scala
+++ src/main/scala/listas/atividades/OrderedSet.scala
@@ -71,9 +71,14 @@ class OrderedSet[A](val comp: (A, A) => Int) {
       * @param set2 Segundo conjunto
       * @return Interseção dos conjuntos
     */
-    def intersect(set1: List[A], set2: List[A]): List[A] = {
-        ???
-    }
+    def intersect(set1: List[A], set2: List[A]): List[A] = (set1, set2) match
+        case (Nil, _) => Nil
+        case (_, Nil) => Nil
+        case (h1 :: t1, h2 :: t2) =>
+            comp(h1, h2) match
+                case result if result < 0 => intersect(t1, set2)
+                case 0 => h1 :: intersect(t1, t2)
+                case _ => intersect(set1, t2)
 
     /**
       * Realiza a diferença entre dois conjuntos ordenados (set1 \ set2).
@@ -98,3 +103,4 @@ class OrderedSet[A](val comp: (A, A) => Int) {
 
     println(setOp.insert(s1, 4))
     println(setOp.union(s1, s2))
+    println(setOp.intersect(s1, s2))
